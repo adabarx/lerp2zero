2caf61657146e1f9d6efafc81a1ea17b645bd5b1
Adamina Barx <adaminabarx@gmail.com>
2025-05-30 15:17:36 -0500

    easing functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,6 @@ use core::f32;
 use nih_plug::prelude::*;
 use std::{collections::VecDeque, sync::Arc};
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
-
 struct Limit2zero {
     params: Arc<Limit2zeroParams>,
     sample_rate: f32,
@@ -30,26 +26,110 @@ enum EnvState {
     Off,
 }
 
-// enum Ratio {
-//     One,
-//     Sqrt2,
-//     Two,
-//     Three,
-//     Four,
-//     Six,
-//     Eight,
-//     Twelve,
-//     Sixteen,
-//     Twentyfour,
-//     Infinite,
-// }
+struct Easing {
+    dir: EaseDirection,
+    shape: EaseShape,
+}
+
+enum EaseDirection {
+    In,
+    Out,
+}
+
+enum EaseShape {
+    Sine,
+    Circle,
+    Exponent,
+    OutBack,
+    Elastic,
+    Bounce,
+}
+
+impl Easing {
+    fn calc(&self, t: f32) -> f32 {
+        use f32::consts::{PI, TAU};
+        use EaseDirection::*;
+        use EaseShape::*;
+        match self.dir {
+            In => match self.shape {
+                Sine => 1.0 - f32::cos((t * PI) / 2.0),
+                Circle => 1.0 - f32::sqrt(1.0 - t.powi(2)),
+                Exponent => {
+                    if t == 0.0 {
+                        0.0
+                    } else {
+                        2_f32.powf(10.0 * t - 10.0)
+                    }
+                }
+                OutBack => {
+                    let c1 = 1.70158;
+                    let c3 = c1 + 1.0;
+
+                    c3 * t * t * t - c1 * t * t
+                }
+                Elastic => {
+                    let c4 = TAU / 3.0;
+
+                    if t == 0.0 {
+                        0.0
+                    } else if t == 1.0 {
+                        1.0
+                    } else {
+                        -2_f32.powf(10.0 * t - 10.0) * f32::sin((t * 10.0 - 10.75) * c4)
+                    }
+                }
+                Bounce => 1.0 - ease_out_bounce(1.0 - t),
+            },
+            Out => match self.shape {
+                Sine => 1.0 - f32::sin((t * PI) / 2.0),
+                Circle => 1.0 - f32::sqrt(1.0 - (t - 1.0).powi(2)),
+                Exponent => {
+                    if t == 1.0 {
+                        1.0
+                    } else {
+                        1.0 - 2_f32.powf(-10.0 * t)
+                    }
+                }
+                OutBack => {
+                    let c1 = 1.70158;
+                    let c3 = c1 + 1.0;
+
+                    1.0 + c3 * (t - 1.0).powi(3) + c1 * (t - 1.0).powi(2)
+                }
+                Elastic => {
+                    let c4 = TAU / 3.0;
+
+                    if t == 0.0 {
+                        0.0
+                    } else if t == 1.0 {
+                        1.0
+                    } else {
+                        2_f32.powf(-10.0 * t) * f32::sin((t * 10.0 - 0.75) * c4) + 1.0
+                    }
+                }
+                Bounce => ease_out_bounce(t),
+            },
+        }
+    }
+}
+
+fn ease_out_bounce(t: f32) -> f32 {
+    let n1 = 7.5625;
+    let d1 = 2.75;
+
+    if t < 1.0 / d1 {
+        n1 * t * t
+    } else if t < 2.0 / d1 {
+        n1 * ((t - 1.5) / d1) * t + 0.75
+    } else if t < 2.5 / d1 {
+        n1 * ((t - 2.25) / d1) * t + 0.9375
+    } else {
+        n1 * ((t - 2.625) / d1) * t + 0.984375
+    }
+}
 
 #[derive(Params)]
 struct Limit2zeroParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[id = "input"]
     pub input: FloatParam,
 
@@ -65,8 +145,8 @@ struct Limit2zeroParams {
     #[id = "release"]
     pub release: FloatParam,
 
-    #[id = "limit2"]
-    pub limit2: FloatParam,
+    #[id = "trim"]
+    pub trim: FloatParam,
 }
 
 impl Default for Limit2zero {
@@ -86,27 +166,17 @@ impl Default for Limit2zero {
 impl Default for Limit2zeroParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
             input: FloatParam::new(
                 "Input",
                 util::db_to_gain(0.0),
                 FloatRange::Skewed {
                     min: util::db_to_gain(-30.0),
                     max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
                     factor: FloatRange::gain_skew_factor(-30.0, 30.0),
                 },
             )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
             .with_smoother(SmoothingStyle::Logarithmic(50.0))
             .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
 
@@ -158,8 +228,8 @@ impl Default for Limit2zeroParams {
             .with_unit("ms")
             .with_value_to_string(formatters::v2s_f32_rounded(2)),
 
-            limit2: FloatParam::new(
-                "limit2",
+            trim: FloatParam::new(
+                "Trim",
                 0.0,
                 FloatRange::Linear {
                     min: -1.0,
@@ -227,6 +297,9 @@ impl Plugin for Limit2zero {
                 let lookahead = self.params.lookahead.value() * self.sample_rate * 0.001;
 
                 if lookahead.ceil() != self.lookahead_len {
+                    // in bitwig i have to set half the latency samples?
+                    // is it like this in other DAWs?
+                    // whyyyyyyyy
                     context.set_latency_samples((lookahead / 2.0).ceil() as u32);
                     self.lookahead_len = lookahead.ceil();
                     self.reset();
@@ -235,7 +308,7 @@ impl Plugin for Limit2zero {
 
                 let attack_amount = self.params.attack.smoothed.next();
                 let input = self.params.input.smoothed.next();
-                let limit2 = self.params.limit2.smoothed.next();
+                let trim = self.params.trim.smoothed.next();
                 let release_sec = self.params.release.smoothed.next() * 0.001;
                 let hold_sec = self.params.hold.smoothed.next() * 0.001;
 
@@ -258,6 +331,9 @@ impl Plugin for Limit2zero {
                     .enumerate()
                     .filter(|(_, s)| s.db > 0.0)
                     .fold(0.0, |rv, (i, s)| {
+                        if s.db + rv < 0.0 {
+                            return rv;
+                        }
                         let t = (self.lookahead_len - i as f32) / self.lookahead_len;
                         let env = lerp(0.0, -1.0 * s.db, t) * attack_amount;
                         f32::min(env, rv)
@@ -341,7 +417,7 @@ impl Plugin for Limit2zero {
                     EnvState::Off => (),
                 }
 
-                *sample = dly_sample * util::db_to_gain_fast(self.envelope + limit2);
+                *sample = dly_sample * util::db_to_gain_fast(self.envelope + trim);
             }
         }
 
